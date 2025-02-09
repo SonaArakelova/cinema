6627145f768d02f3b1fe1346ff5194fc2bc1056e
SonaArakelova <arakelovasona@gmail.com>
2025-02-09 23:28:25 +0400

    Updates

---
+++ precache-manifest.4bffee74036de06ed7c119960e961273.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d285c302212dd627d7dd7edaf6dbb103",
+    "url": "/cinema/index.html"
+  },
+  {
+    "revision": "2119d3ea5caa786ae393",
+    "url": "/cinema/static/css/2.90cab898.chunk.css"
+  },
+  {
+    "revision": "e5c40fe00b140d6d2089",
+    "url": "/cinema/static/css/main.fa220491.chunk.css"
+  },
+  {
+    "revision": "2119d3ea5caa786ae393",
+    "url": "/cinema/static/js/2.3d88aba1.chunk.js"
+  },
+  {
+    "revision": "e5c40fe00b140d6d2089",
+    "url": "/cinema/static/js/main.35408b4f.chunk.js"
+  },
+  {
+    "revision": "b867071ff6b861045f0a",
+    "url": "/cinema/static/js/runtime~main.069b81f7.js"
+  },
+  {
+    "revision": "ba49e844892321d8540ea3b7c088cf97",
+    "url": "/cinema/static/media/bootstrap-icons.ba49e844.woff"
+  },
+  {
+    "revision": "cc1e5eda776be5f0ff614285c31d4892",
+    "url": "/cinema/static/media/bootstrap-icons.cc1e5eda.woff2"
+  }
+]);
